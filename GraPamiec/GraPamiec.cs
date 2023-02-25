47d6f65fc250cc40f16a91c004325bb7df06167e
Tomasz Grzywacz <tomaszgrzywacz92@gmail.com>
2023-02-25 15:09:13 +0100

    graPamiec

--- GraPamiec/GraPamiec.cs
+++ GraPamiec/GraPamiec.cs
@@ -21,52 +21,15 @@ namespace GraPamiec
             UzupelnijPanel();            
         }
 
-        public List<Karta> GenerujKarty()
-        {
-            var obrazki = ObrazkiRepozytorium.PobierzObrazki();
-
-            var karty = new List<Karta>();
-
-            foreach (var obrazek in obrazki)
-            {
-                var id = Guid.NewGuid();
-
-                var karta1 = new Karta(id, obrazek);
-                karty.Add(karta1);
-
-                var karta2 = new Karta(id, obrazek);
-                karty.Add(karta2);
-            }
-
-            return karty;
-        }
-
         public void UzupelnijPanel()
         {
-            var karty = GenerujKarty();
-            var kolumny = 4;
-            var wiersze = 4;
-            Random generator = new Random();
+            var karta = new Karta(Guid.Empty, ObrazkiRepozytorium.PobierzObrazki()[0]);
 
-            for (int x = 0; x < kolumny; x++)
-            {
-                for (int y = 0; y < wiersze; y++)
-                {
-                    var indeksKarty = generator.Next(0, karty.Count - 1);
-                    var karta = karty[indeksKarty];
-                    karta.Location = new Point(x * karta.Width, y * karta.Height);
-                    karta.Click += KartaClicked;
-                    panelGry.Controls.Add(karta);
-                    karty.Remove(karta);
-                }
-            }
-        }
+            karta.Location = new Point(0, 0);
 
-        private void KartaClicked(object sender, EventArgs e)
-        {
-            var karta = (Karta)sender;
-            MessageBox.Show(karta.Id.ToString());
             karta.Zakryj();
+
+            panelGry.Controls.Add(karta);
         }
     }
 }
